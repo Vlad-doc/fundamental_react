2e6a967928c53887d329c53645bc98a138e250b6
Your Name <you@example.com>
2022-05-26 13:37:13 +0300

    add component postItme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react"
 import ClassCounter from "./components/class-counter"
 import { Counter } from "./components/counter"
+import PostItem from "./components/post-item"
+import "./styles/app.css"
 
 function App() {
   const [value, setValue] = useState("text input")
 
   return (
-    <div>
-      <Counter />
-      <ClassCounter />
+    <div className="app">
+      <PostItem />
     </div>
   )
 }
