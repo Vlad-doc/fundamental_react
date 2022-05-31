747f333ed18ded331e2a19a0ecd428ed0c6c9128
Your Name <you@example.com>
2022-05-31 10:48:52 +0300

    add axios for get posts from placeholder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import axios from "axios"
+import React, { useEffect, useState } from "react"
 import PostFilter from "./components/post-filter"
 import PostForm from "./components/post-form"
 import PostList from "./components/post-list"
@@ -12,6 +13,16 @@ function App() {
   const [filter, setFilter] = useState({ sort: "", querry: "" })
   const [modal, setModal] = useState(false)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.querry)
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts",
+      )
+      setPosts(response.data)
+    }
+    fetchPosts()
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
