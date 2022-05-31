ed5f7e1db755163c5d4e61103103b126507e9b21
Your Name <you@example.com>
2022-05-31 16:01:09 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./components/UI/navbar/navbar"
 import About from "./pages/about"
 import Posts from "./pages/posts"
 import "./styles/app.css"
@@ -7,6 +8,7 @@ import "./styles/app.css"
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/about" element={<About />}></Route>
         <Route path="/posts" element={<Posts />}></Route>
